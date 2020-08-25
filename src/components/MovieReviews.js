74d1f0e34821451171c5fa5c732ef3904a225888
Megan Horsfield <meganthadev@gmail.com>
2020-08-25 07:04:23 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Review = ({ byline, headline, summary_short }) => {
     return (
       <div className="review">
-      
+        <li>
+          <h3>{headline}</h3>
+          <h4>By: {byline}</h4>
+          <p>{summary_short}</p>
+        </li>
+        
       
       )
  }
