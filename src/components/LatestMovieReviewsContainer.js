8b2a418563184aa8e2fa6b223b5ddb0758d2fe8d
Megan Horsfield <meganthadev@gmail.com>
2020-08-25 07:07:22 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,14 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: []
+    };
+  }
+  
+  
